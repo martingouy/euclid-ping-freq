4e8f1a850b1209a26e9a1f1a3263af671fab66b7
mgouy <mgouy@euclidanalytics.com>
2019-01-11 17:27:48 -0800

    f

--- simple-leo.py
+++ simple-leo.py
@@ -3,4 +3,4 @@ from scapy.all import *
 def pkt_callback(pkt):
     pkt.show() # debug statement
 
-sniff(iface="wlan1", prn=pkt_callback, store=0)
+sniff(iface="wlan1mon", prn=pkt_callback, store=0)
