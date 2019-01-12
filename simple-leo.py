bc0cebf6fa2b675ca7d96f78f0573c4f234ff506
mgouy <mgouy@euclidanalytics.com>
2019-01-11 17:25:57 -0800

    f

--- simple-leo.py
+++ simple-leo.py
@@ -3,4 +3,4 @@ from scapy.all import *
 def pkt_callback(pkt):
     pkt.show() # debug statement
 
-sniff(iface="<My Interface>", prn=pkt_callback, filter="tcp", store=0)
+sniff(iface="wlan1", prn=pkt_callback, store=0)
